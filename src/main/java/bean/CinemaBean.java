662c4b837bca4bc1ed5919e6632ffcde76a6e8cd
YIIIIIACH <1997marc212@gmail.com>
2023-10-05 20:56:22 +0800

    add bean
    file:///home/yiiiiiach/WorkSpace/WeExpose_Workspace/WeExpose/src/main/java/bean/CinemaBean.java

---
+++ src/main/java/bean/CinemaBean.java
@@ -0,0 +1,26 @@
+package bean;
+
+public class CinemaBean {
+	private int cinemaId;
+	private String cinemaName;
+	private String cinemaLocation;
+	public int getCinemaId() {
+		return cinemaId;
+	}
+	public void setCinemaId(int cinemaId) {
+		this.cinemaId = cinemaId;
+	}
+	public String getCinemaName() {
+		return cinemaName;
+	}
+	public void setCinemaName(String cinemaName) {
+		this.cinemaName = cinemaName;
+	}
+	public String getCinemaLocation() {
+		return cinemaLocation;
+	}
+	public void setCinemaLocation(String cinemaLocation) {
+		this.cinemaLocation = cinemaLocation;
+	}
+	
+}
